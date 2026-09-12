521e5e9b0cfdfe2b49334d9c497cb6db116686fc
omersusin <omersusin@users.noreply.github.com>
2026-09-12 16:10:43 +0300

    fix: CameraX direct versions 1.3.0, no BOM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -89,7 +89,6 @@ dependencies {
     testImplementation(libs.junit)
     implementation(libs.zxing.core)
     implementation(libs.material.color.utilities)
-    implementation(platform(libs.camera.bom))
     implementation(libs.camera.core)
     implementation(libs.camera.camera2)
     implementation(libs.camera.lifecycle)
